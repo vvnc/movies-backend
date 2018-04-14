c10e097c8ec9096156bc4ba631161413646e2374
vvnc <vincent.roth@yandex.ru>
2018-04-14 16:57:55 +0300

    Fix authenticatioin middleware order
    authentication must go before MVC in order to work properly with MVC
    controllers.

--- MoviesBackend/Startup.cs
+++ MoviesBackend/Startup.cs
@@ -45,19 +45,17 @@ namespace MoviesBackend
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IHostingEnvironment env)
     {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      app.UseAuthentication();
       app.UseMvc(routes =>
       {
         routes.MapRoute(
             name: "default",
             template: "{controller=Home}/{action=Index}/{id?}");
       });
-
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-      app.UseAuthentication();
-      app.UseMvc();
     }
   }
 }
