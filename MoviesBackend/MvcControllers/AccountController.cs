69891677c1016659837dc3d23b6871767db09952
vvnc <vincent.roth@yandex.ru>
2018-04-14 17:00:11 +0300

    Implement login page

--- MoviesBackend/MvcControllers/AccountController.cs
+++ MoviesBackend/MvcControllers/AccountController.cs
@@ -90,4 +90,39 @@ public class AccountController : Controller
 
     return Content("Email confirmed, you can now log in");
   }
+
+  public IActionResult Login()
+  {
+    return View();
+  }
+
+  [HttpPost]
+  public async Task<IActionResult> Login(string email, string password, bool rememberMe)
+  {
+    var user = await _userManager.FindByEmailAsync(email);
+    if (user == null)
+    {
+      ModelState.AddModelError(string.Empty, "Invalid login");
+      return View();
+    }
+    if (!user.EmailConfirmed)
+    {
+      ModelState.AddModelError(string.Empty, "Confirm your email first");
+      return View();
+    }
+
+    var passwordSignInResult = await _signInManager.PasswordSignInAsync(
+      user,
+      password,
+      isPersistent: rememberMe,
+      lockoutOnFailure: false
+    );
+    if (!passwordSignInResult.Succeeded)
+    {
+      ModelState.AddModelError(string.Empty, "Invalid login");
+      return View();
+    }
+
+    return Redirect("~/");
+  }
 }
