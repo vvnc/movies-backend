5444691ec3de0318c8aa3e263b08cca755a2ece5
vvnc <vincent.roth@yandex.ru>
2018-04-30 16:57:54 +0300

    Add enpty JWT token generation for every user

---
+++ MoviesBackend/Exceptions/JwtException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesBackend.Exceptions
+{
+  public class JwtException : Exception
+  {
+    public JwtException(string msg)
+      : base(msg)
+    { }
+  }
+}
